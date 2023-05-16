7279cc2101944cf93c3ceb0a1a3e363d852f1bb8
helpingstar <iamhelpingstar@gmail.com>
2023-05-17 00:21:41 +0900

    first commit

---
+++ random_policy.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+import gym_game2048
+
+env = gym.make("gym_game2048/Game2048-v0", render_mode="human")
+
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+       observation, info = env.reset()
+env.close()
