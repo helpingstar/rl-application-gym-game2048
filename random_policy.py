99618b12998e4988fa0944bb19eb268e6eb32fd8
helpingstar <54899900+helpingstar@users.noreply.github.com>
2023-05-17 22:12:34 +0900

    add random_policy

--- random_policy.py
+++ random_policy.py
@@ -5,7 +5,7 @@ env = gym.make("gym_game2048/Game2048-v0", render_mode="human")
 
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-    action = env.action_space.sample()  # this is where you would insert your policy
+    action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
        observation, info = env.reset()
