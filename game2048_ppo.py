b2b89517d3a61a6b0ffb9aab539c75d2af303eda
helpingstar <iamhelpingstar@gmail.com>
2023-05-19 21:47:38 +0900

    update ppo code

--- game2048_ppo.py
+++ game2048_ppo.py
@@ -4,10 +4,11 @@ import os
 import random
 import time
 from distutils.util import strtobool
+from tqdm import tqdm
 
 import gym_game2048
-from gym_game2048.wrappers import PreprocessForTensor, Normalize2048, RewardConverter
-from gymnasium.wrappers import FlattenObservation, TimeLimit
+from gym_game2048.wrappers import PreprocessForTensor, Normalize2048, RewardConverter, RewardByScore
+from gymnasium.wrappers import FlattenObservation, TimeLimit, TransformReward
 import gymnasium as gym
 import numpy as np
 import torch
@@ -28,27 +29,27 @@ def parse_args():
         help="if toggled, `torch.backends.cudnn.deterministic=False`")
     parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="if toggled, cuda will be enabled by default")
-    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=False,
+    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=False,
         help="if toggled, this experiment will be tracked with Weights and Biases")
     parser.add_argument("--wandb-project-name", type=str, default="cleanRL",
         help="the wandb's project name")
     parser.add_argument("--wandb-entity", type=str, default=None,
         help="the entity (team) of wandb's project")
-    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="gym_game2048/Game2048-v0",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=20000000,
+    parser.add_argument("--total-timesteps", type=int, default=500000000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
-    parser.add_argument("--num-envs", type=int, default=8,
+    parser.add_argument("--num-envs", type=int, default=16,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
-    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
     parser.add_argument("--gamma", type=float, default=0.99,
         help="the discount factor gamma")
@@ -72,6 +73,13 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
+    
+    parser.add_argument("--goal", type=int, default=2048,
+        help="goal")
+    parser.add_argument("--save-model", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="whether to save model into the `runs/{run_name}` folder")
+    parser.add_argument("--load-model", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="whether to load model `runs/{run_name}` folder")
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
@@ -82,13 +90,16 @@ def parse_args():
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array", goal=64)
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+            env = gym.make(env_id, render_mode="rgb_array", goal=args.goal)
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", episode_trigger=lambda x: (x % 500 == 0), disable_logger=True)
         else:
-            env = gym.make(env_id)
+            env = gym.make(env_id, goal=args.goal)
         #### Add Custom Wrappers ###
-        # env = TimeLimit(env, max_episode_steps=10000)
-        env = RewardConverter(env, terminated_reward=5, other=-0.001)
+        # env = TimeLimit(env, max_episode_steps=3000)
+        # env = RewardConverter(env, goal=6, fail=-5, other=-0.0001)
+
+        env = RewardByScore(env)
+        env = TransformReward(env, lambda r: r * 0.1)
         env = Normalize2048(env)
         env = FlattenObservation(env)
         #############################
@@ -188,7 +199,7 @@ if __name__ == "__main__":
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
 
-    for update in range(1, num_updates + 1):
+    for update in tqdm(range(1, num_updates + 1)):
         # Annealing the rate if instructed to do so.
         if args.anneal_lr:
             frac = 1.0 - (update - 1.0) / num_updates
@@ -219,10 +230,11 @@ if __name__ == "__main__":
                 # Skip the envs that are not done
                 if info is None:
                     continue
-                print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                # print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
                 writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
                 writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
                 writer.add_scalar("charts/score", info["score"], global_step)
+                writer.add_scalar("charts/max_number", info["max"], global_step)
 
         # bootstrap value if not done
         with torch.no_grad():
@@ -316,8 +328,16 @@ if __name__ == "__main__":
         writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
         writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
         writer.add_scalar("losses/explained_variance", explained_var, global_step)
-        print("SPS:", int(global_step / (time.time() - start_time)))
+        # print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+        if args.save_model:
+            if update % 20000 == 0:
+                model_path = f"runs/{run_name}/cleanrl_{args.exp_name}_{update}.pt"
+                torch.save(agent.state_dict(), model_path)
+                print(f"model saved to {model_path}")
+            
+
 
     envs.close()
     writer.close()
